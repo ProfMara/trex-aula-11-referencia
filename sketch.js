19d1983e6e8de8926034597171c354831a643aec
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-02-21 18:55:28 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 var soloSprite, soloImagem;
 //é aqui que cria a variável
 var trexSprite, trexAnimacao;
+
 //nessa função, carrega-se arquivos de mídia
 function preload() {
-
     //é aqui que carrega a animação
     trexAnimacao = loadAnimation("trex1.png","trex2.png","trex3.png");
 
@@ -12,22 +12,28 @@ function preload() {
 
 function setup() {
     createCanvas(600, 200);
-    
-
-    //é aqui que cria a sprite
+    //é aqui que cria as sprites
     trexSprite = createSprite(50,170,50,50);
     trexSprite.addAnimation("correndo",trexAnimacao);
     trexSprite.scale=0.5;
+    
+    
 
 }
 
 
 function draw() {
-    background("cyan")
-    
+    //pinta o fundo de uma cor
+    background("cyan");
+    //verifica se a pessoa apertou a tecla espaço
     if(keyDown("space")){
-      trexSprite.velocityY = -10;
+        //dá velocidade para o trex voar
+        trexSprite.velocityY = -10;
     }
+    //esse código dá gravidade para o trex cair
     trexSprite.velocityY += 0.8;
+    
+    
+    
     drawSprites();
 }
